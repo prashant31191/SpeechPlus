e8958525211f4693a2b6904345c0909162b91d0b
Alex Shavkunov <aleeexshavkunov@gmail.com>
2017-07-28 17:01:29 +0500

    Add SpeechFragment.java and fragment_speech.xml

---
+++ app/src/main/java/com/shavkunov/razvitie/samo/SpeechFragment.java
@@ -0,0 +1,37 @@
+package com.shavkunov.razvitie.samo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class SpeechFragment extends Fragment {
+
+    private Unbinder unbinder;
+
+    @BindView(R.id.speech_recycler_view)
+    RecyclerView speechRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_speech, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        speechRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
