aa6215ccc0104f65738f99aa984405ab167693ff
Alex Shavkunov <aleeexshavkunov@gmail.com>
2017-07-28 16:57:10 +0500

    Add SingleFragmentActivity.java

--- app/src/main/java/com/shavkunov/razvitie/samo/SpeechActivity.java
+++ app/src/main/java/com/shavkunov/razvitie/samo/SpeechActivity.java
@@ -8,6 +8,6 @@ public class SpeechActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_speech);
+        setContentView(R.layout.activity_fragment);
     }
 }
