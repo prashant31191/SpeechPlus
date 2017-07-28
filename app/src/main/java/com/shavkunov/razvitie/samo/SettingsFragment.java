886b4c366170a76586861166b0deb0c0ac2dc1fc
Alex Shavkunov <aleeexshavkunov@gmail.com>
2017-07-28 18:52:25 +0500

    Add two fragments - FavoritesFragment and SettingsFragment

---
+++ app/src/main/java/com/shavkunov/razvitie/samo/SettingsFragment.java
@@ -0,0 +1,21 @@
+package com.shavkunov.razvitie.samo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsFragment extends Fragment {
+
+    public static Fragment newInstance() {
+        return new SettingsFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
+    }
+}
