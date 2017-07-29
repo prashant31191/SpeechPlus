0fb04251bfc7fa429500449179da08aaeb3a9cd8
Alex Shavkunov <aleeexshavkunov@gmail.com>
2017-07-29 13:47:14 +0500

    Add CardData.java

---
+++ app/src/main/java/com/shavkunov/razvitie/samo/CardData.java
@@ -0,0 +1,28 @@
+package com.shavkunov.razvitie.samo;
+
+public class CardData {
+
+    private int imageResId;
+    private String title;
+
+    public CardData(int imageResId, String title) {
+        this.imageResId = imageResId;
+        this.title = title;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public void setImageResId(int imageResId) {
+        this.imageResId = imageResId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
